fd7cc1214c85164e273c299f9fdc060943db51a1
artman5545 <artman5545@163.com>
2020-02-11 19:15:58 +0800

    ssdgwerqwer
    Signed-off-by: artman5545 <artman5545@163.com>

---
+++ src/components/Custom/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div><input v-model="ck" />
+    <ul>
+      <li v-for="(item,index) in list" :key="index">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ccii',
+  props: {
+    msg: {
+      type: String,
+      default: 'test msg'
+    }
+  },
+  data: function () {
+    return {
+      ck: 'sss',
+      list: []
+    }
+  },
+  beforeCreate: function () {},
+  created: function () {
+    this.init()
+  },
+  beforeMount: function () {
+
+  },
+  mounted: function () {
+
+  },
+  methods: {
+    init: function () {
+      var ss = this
+      window.setInterval(function () {
+        ss.list.push(new Date())
+      }, 100)
+    }
+  }
+}
+
+</script>
